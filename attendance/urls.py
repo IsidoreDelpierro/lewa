109cf6343cdcae77333cd768e1d4eb4d72754174
Isidore DelPierro <fontadore@gmail.com>
2022-02-20 13:20:13 +0100

    Basic Auth - register - login - logout

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,12 +1,16 @@
 """lewa URL Configuration
-    Attendance module for our virtual school 
+    Attendance module for our virtual school
 """
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+#from attendance.views import register
 
 app_name = "attendance"
 
 urlpatterns = [
     path("", views.attendance, name="attendance"),
-
+    path("register/", views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="attendance/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="attendance/logout.html"), name="logout"),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def attendance(request):
-    return HttpResponse("Wow this is an <stron>awesome</strong> app")
+    return HttpResponse("Wow this is an <strong>awesome</strong> app")
+
+def register(request):
+    if request.method == "POST":
+        print("POST Request received")
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            print("Form is Valid")
+            form.save()
+    else:
+        print("GET Request received")
+        form = UserRegistrationForm()
+
+    return render(request, "attendance/register.html", {'form': form})
